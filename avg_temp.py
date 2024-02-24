5695d56cce0432b882a853820c1d14c50f2c3c72
Wai Chan <wcc325@nyu.edu>
2024-02-23 19:46:34 -0500

    updated avg temp to function

--- avg_temp.py
+++ avg_temp.py
@@ -1,13 +1,11 @@
 from array import *
-days = int (input ("How many days do you want to calculate? "))
-temp_arr = array('i')
-for i in range(days):
-    temp = int (input ("Highest temp for day " + str(i + 1) + " is? "))
-    temp_arr.append(temp)
-
-avg = sum(temp_arr) / (len(temp_arr))
-print ("Average = " + str(avg))
-
-res = sum(i > avg for i in temp_arr)
-
-print ("Average temp is higher than " + str(res) + " days!")
+def avg_temp():
+    days = int (input ("How many days do you want to calculate? "))
+    temp_arr = array('i')
+    for i in range(days):
+        temp = int (input ("Highest temp for day " + str(i + 1) + " is? "))
+        temp_arr.append(temp)
+    avg = sum(temp_arr) / (len(temp_arr))
+    print ("Average = " + str(avg))
+    res = sum(i > avg for i in temp_arr)
+    print ("Average temp is higher than " + str(res) + " days!")

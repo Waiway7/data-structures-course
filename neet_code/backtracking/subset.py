3ca7d5dc045441454d26a5ca6be8e7edee54377f
Wai Chan <wcc325@nyu.edu>
2024-03-12 19:32:18 -0400

    word search

---
+++ neet_code/backtracking/subset.py
@@ -0,0 +1,27 @@
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         subset = []
+
+#         def dfs(i):
+#             if i >= len(nums):
+#                 res.append(subset[:])
+#                 return
+#             subset.append(nums[i])
+#             dfs(i + 1)
+#             subset.pop()
+#             dfs(i + 1)
+
+#         dfs(0)
+#         return res
+
+def help(arr, num, i):
+    if len(arr) == i:
+        return
+    arr.append(num[i])
+    help(arr, num, i+1)
+    print (arr)
+    return arr
+
+num = [1,2,3,4]
+print (help([], num, 0))
